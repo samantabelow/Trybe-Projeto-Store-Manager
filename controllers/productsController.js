6b75b0e2b4144ad8565d2eb8fdbe43f74dcfe99b
samantabelow <samanta.below@gmail.com>
2021-04-14 11:47:20 -0300

    Pós plantão do Zambelli

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -7,23 +7,27 @@ const addProduct = async (req, res, next) => {
   const {name, quantity} = req.body;
   const minLength = 5;
   if (name.length < minLength) {
-    next({ status: 422, message: '"name" length must be at least 5 characters long'});
+    return next({
+      status: 422, message: '"name" length must be at least 5 characters long'
+    });
   }
-  if (Products.checkForProductName(name)) {
-    next({ status: 422, message: 'Product already exists'});
+  const nameCheck = await Products.checkForProductName(name);
+  console.log(nameCheck);
+  if (nameCheck) {
+    return next({ status: 422, message: 'Product already exists'});
   }
   if (quantity < 1) {
-    next({ status: 422, message: '"quantity" must be larger than or equal to 1'});
+    return next({ status: 422, message: '"quantity" must be larger than or equal to 1'});
   }
   if (typeof quantity !== 'number') {
-    next({ status: 422, message: '"quantity" must be a number'});
+    return next({ status: 422, message: '"quantity" must be a number'});
   }
   try {
     const results = await Products.addProduct(name, quantity);
 
     res.status(SUCCESS).json(results);
   } catch (err) {
-    console.log("oi");
+    console.log('oi');
     console.log(err);
     res.status(FAIL).json({ message: err.message });
   }
--- models/productsModel.js
+++ models/productsModel.js
@@ -2,19 +2,13 @@ const connection = require('../config/conn');
 
 const { ObjectId } = require('mongodb');
 
-const getProductsList = async () => {
-  await connection().then((db) => db.collection('products'));
-};
+const getProductsList = async () => connection().then((db) => db.collection('products'));
 
-const checkForProductName = async (name) => {
-  return await getProductsList()
-    .then((products) => products.findOne( {name} ));
-};
+const checkForProductName = async (name) => getProductsList()
+  .then((products) => products.findOne( {name} ));
 
-const addProduct = async (name, quantity) => {
-  return await connection()
-    .then((db) => db.collection('products').insertOne({name, quantity}));
-};
+const addProduct = async (name, quantity) => connection()
+  .then((db) => db.collection('products').insertOne({name, quantity}));
 
 module.exports = {
   addProduct,
